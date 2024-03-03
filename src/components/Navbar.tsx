cfd18d004c629db5b244ea2e8604ce3f00c66e50
Efraim <efraimalhazov@gmail.com>
2024-03-03 10:11:23 +0200

    added translation

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,12 @@
+import { useTranslation } from 'react-i18next';
 import KlipsLogo from '../assets/klips-ligh-desktop.svg';
 
 const Navbar = () => {
+  const { t } = useTranslation();
   return (
     <div className="navbar">
       <img className="logo" src={KlipsLogo} alt="klips-logo" />
-      <h1 className="title">Payment Verification</h1>
+      <h1 className="title">{t('paymentVerification')}</h1>
     </div>
   );
 };
