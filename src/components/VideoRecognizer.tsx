cc008e5f7f80bcf39a55706378fbd6f878be3f1f
Efraim <efraimalhazov@gmail.com>
2024-03-06 17:23:19 +0200

    modified the back end endpoint

--- src/components/VideoRecognizer.tsx
+++ src/components/VideoRecognizer.tsx
@@ -7,8 +7,17 @@ import Navbar from './Navbar';
 import CompletedSvg from './CompletedSvg';
 import FailedSvg from './FailedSvg';
 import { EMode } from '../types/env';
-import { getLicenseKeyByEnvironment } from '../utils';
-import axios from 'axios';
+import {
+  GetActionCodeResponseType,
+  VERIFICATION_TYPES,
+  VerifyActionCodeResponseType,
+  getActionCode,
+  getLicenseKeyByEnvironment,
+  initializeBlinkCardSDK,
+  validateActionCode,
+  verifyPaymentMethod,
+} from '../utils';
+import { AxiosResponse } from 'axios';
 
 const VideoRecognizer = () => {
   const [isShown, setIsShown] = useState<boolean>(false);
@@ -22,149 +31,136 @@ const VideoRecognizer = () => {
   const language = searchParams.get('language');
   const currEnvironment = searchParams.get('env') as EMode;
   const licenseKey = getLicenseKeyByEnvironment(currEnvironment);
-
   const userToken = searchParams.get('userToken');
+  const paymentMethodId = searchParams.get('paymentID')?.toString() as string;
 
-  type PaymentMethodRequestType = {
-    bin: string;
-    lastDigits: string;
-    expiryYear: number;
-    expiryMonth: number;
-    cardHolder: string;
-    token: string | null;
-  };
+  const [continueToScan, setContinueToScan] = useState<boolean>(true);
 
-  // Set the language based on the language of front app
   useEffect(() => {
     i18n.changeLanguage(language as string | undefined);
   }, [language]);
 
+  useEffect(() => {
+    getActionCode({ action: VERIFICATION_TYPES.PM_VERIFICATION, token: userToken as string })
+      .then(({ data }: AxiosResponse<GetActionCodeResponseType>) => {
+        if (data.expiryTime > 0) {
+          validateActionCode({
+            action: VERIFICATION_TYPES.PM_VERIFICATION,
+            code: data.code,
+            token: userToken as string,
+          })
+            .then(({ data }: AxiosResponse<VerifyActionCodeResponseType>) => {
+              if (data.status) {
+                setContinueToScan(true);
+              } else {
+                setUserMessage('codeFail');
+                setFailed(true);
+              }
+            })
+            .catch(() => {
+              setUserMessage('codeFail');
+              setFailed(true);
+            });
+        }
+      })
+      .catch(() => {
+        setUserMessage('codeFail');
+        setFailed(true);
+      });
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
   // Init the blinkcard recognizer and begin recognition
   useEffect(() => {
-    const verifyPaymentMethod = async ({
-      bin,
-      lastDigits,
-      expiryMonth,
-      expiryYear,
-      cardHolder,
-      token,
-    }: PaymentMethodRequestType) =>
-      axios.post(
-        'v1/payment/verifyPaymentMethod',
-        {
-          bin,
-          lastDigits,
-          expiryYear,
-          expiryMonth,
-          cardHolder,
-        },
-        {
-          headers: {
-            Authorization: `Bearer ${token}`,
-          },
-        }
-      );
-
-    const initializeBlinkCardSDK = async () => {
-      // Check if browser is supported
-      if (BlinkCardSDK.isBrowserSupported()) {
-        const loadSettings = new BlinkCardSDK.WasmSDKLoadSettings(
-          import.meta.env.VITE_MICROBLINK_LICENSE_KEY_DEV
-        );
-
-        try {
-          const wasmSDK = await BlinkCardSDK.loadWasmModule(loadSettings);
-          setIsShown(true);
-          // The SDK was initialized successfully, you can save the wasmSDK for future use
-          return wasmSDK;
-        } catch (error) {
-          // Error happened during the initialization of the SDK
-          setFailed(true);
-          setUserMessage(t('errorInitialization'));
-          console.error('Error during the initialization of the BlinkCard SDK:', error);
-        }
-      } else {
-        setUserMessage(t('browserNotSupported'));
-        console.log('This browser is not supported by the BlinkCard SDK!');
-      }
-    };
-
-    initializeBlinkCardSDK()
-      .then(async (wasmSDK) => {
-        const callbacks = {
-          onFirstSideResult: () => setUserMessage(t('flip')),
-        };
-        const recognizer = await BlinkCardSDK.createBlinkCardRecognizer(wasmSDK);
-        const recognizerRunner = await BlinkCardSDK.createRecognizerRunner(
-          wasmSDK,
-          [recognizer],
-          false,
-          callbacks
-        );
-        return { recognizerRunner, recognizer };
+    if (continueToScan)
+      initializeBlinkCardSDK({
+        setIsShown,
+        setUserMessage,
+        browserErrorString: t('browserNotSupported'),
+        errorString: t('errorInitialization'),
+        setFailed,
       })
-      .then(async ({ recognizerRunner, recognizer }) => {
-        const cameraFeed = document.getElementById('camera-feed') as HTMLVideoElement;
-        const videoRecognizer =
-          await BlinkCardSDK.VideoRecognizer.createVideoRecognizerFromCameraStream(
-            cameraFeed,
-            recognizerRunner
+        .then(async (wasmSDK) => {
+          const callbacks = {
+            onFirstSideResult: () => setUserMessage(t('flip')),
+          };
+          const recognizer = await BlinkCardSDK.createBlinkCardRecognizer(wasmSDK);
+          const recognizerRunner = await BlinkCardSDK.createRecognizerRunner(
+            wasmSDK,
+            [recognizer],
+            false,
+            callbacks
           );
-        return { videoRecognizer, recognizer, recognizerRunner };
-      })
-      .then(async ({ videoRecognizer, recognizer, recognizerRunner }) => {
-        const processResult = await videoRecognizer.recognize();
+          return { recognizerRunner, recognizer };
+        })
+        .then(async ({ recognizerRunner, recognizer }) => {
+          const cameraFeed = document.getElementById('camera-feed') as HTMLVideoElement;
+          const videoRecognizer =
+            await BlinkCardSDK.VideoRecognizer.createVideoRecognizerFromCameraStream(
+              cameraFeed,
+              recognizerRunner
+            );
+          return { videoRecognizer, recognizer, recognizerRunner };
+        })
+        .then(async ({ videoRecognizer, recognizer, recognizerRunner }) => {
+          const processResult = await videoRecognizer.recognize();
 
-        if (processResult !== BlinkCardSDK.RecognizerResultState.Empty) {
-          const blinkCardResult = await recognizer.getResult();
-          if (blinkCardResult.state !== BlinkCardSDK.RecognizerResultState.Empty) {
-            // Remove spaces from the card number
-            const trimmedCardNumber = blinkCardResult?.cardNumber.replace(/\s+/g, '');
-            // Extract the needed card numbers
-            const bin = trimmedCardNumber.substring(0, 6);
-            const lastDigits = trimmedCardNumber.substring(trimmedCardNumber.length - 4);
-            const expiryMonth = blinkCardResult?.expiryDate?.month;
-            const expiryYear = blinkCardResult?.expiryDate?.month;
-            const cardHolder = blinkCardResult?.owner;
-            //  TODO: add server request to send data.
+          if (processResult !== BlinkCardSDK.RecognizerResultState.Empty) {
+            const blinkCardResult = await recognizer.getResult();
+            if (blinkCardResult.state !== BlinkCardSDK.RecognizerResultState.Empty) {
+              // Remove spaces from the card number
+              // const trimmedCardNumber = blinkCardResult?.cardNumber.replace(/\s+/g, '');
+              // Extract the needed card numbers
+              // const bin = trimmedCardNumber.substring(0, 6);
+              // const lastDigits = trimmedCardNumber.substring(trimmedCardNumber.length - 4);
+              // const expiryMonth = blinkCardResult?.expiryDate?.month;
+              // const expiryYear = blinkCardResult?.expiryDate?.month;
+              // const cardHolder = blinkCardResult?.owner;
+              try {
+                await verifyPaymentMethod({
+                  paymentMethodId: '129',
+                  bin: '411111',
+                  lastDigits: '1111',
+                  expiryMonth: 12,
+                  expiryYear: 26,
+                  cardHolder: 'test test',
+                  token: userToken,
+                }).then((res) => {
+                  if (res.data.message === 'Payment method already verified.') {
+                    setUserMessage('');
+                  }
+                });
 
-            try {
-              await verifyPaymentMethod({
-                bin,
-                lastDigits,
-                expiryMonth,
-                expiryYear,
-                cardHolder,
-                token: userToken,
-              });
-              setUserMessage(t('completed'));
-              setCompleted(true);
-            } catch (err) {
-              console.log('err', err);
-              setFailed(true);
-              setUserMessage(t('failed'));
+                setUserMessage(t('completed'));
+                setCompleted(true);
+              } catch (err) {
+                setFailed(true);
+                setUserMessage(t('failed'));
+              }
             }
+
+            videoRecognizer?.releaseVideoFeed();
+            setIsShown(false);
+            // Release memory on WebAssembly heap used by the RecognizerRunner
+            recognizerRunner?.delete();
+
+            // Release memory on WebAssembly heap used by the recognizer
+            recognizer?.delete();
+          } else {
+            setUserMessage(t('errorExtracting'));
+            videoRecognizer?.releaseVideoFeed();
+            setIsShown(false);
+            // Release memory on WebAssembly heap used by the RecognizerRunner
+            recognizerRunner?.delete();
+
+            // Release memory on WebAssembly heap used by the recognizer
+            recognizer?.delete();
           }
-
-          videoRecognizer?.releaseVideoFeed();
-          setIsShown(false);
-          // Release memory on WebAssembly heap used by the RecognizerRunner
-          recognizerRunner?.delete();
-
-          // Release memory on WebAssembly heap used by the recognizer
-          recognizer?.delete();
-        } else {
-          setUserMessage(t('errorExtracting'));
-          videoRecognizer?.releaseVideoFeed();
-          setIsShown(false);
-          // Release memory on WebAssembly heap used by the RecognizerRunner
-          recognizerRunner?.delete();
-
-          // Release memory on WebAssembly heap used by the recognizer
-          recognizer?.delete();
-        }
-      });
-  }, [licenseKey, t, userToken]);
+        })
+        .catch(() => {
+          setUserMessage(t('errorInitialization'));
+        });
+  }, [licenseKey, t, userToken, continueToScan, paymentMethodId]);
 
   const isTransparent = isShown ? { color: 'white' } : { color: 'black' };
 
@@ -172,7 +168,7 @@ const VideoRecognizer = () => {
     <>
       <Navbar />
       <div id="screen-scanning">
-        <video id="camera-feed" playsInline></video>
+        {continueToScan && <video id="camera-feed" playsInline></video>}
         {!completed && (
           <p id="camera-guides" style={isTransparent}>
             {t('cameraGuide')}
@@ -181,7 +177,7 @@ const VideoRecognizer = () => {
 
         <div id="flip-guides">
           <p style={isTransparent} className={completed ? 'completed-text' : 'text'}>
-            {userMessage} 15:00
+            {userMessage}
           </p>
           <CompletedSvg show={completed} />
           <FailedSvg show={failed} />
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,7 @@
+import axios from 'axios';
 import { EMode } from '../types/env';
+import * as BlinkCardSDK from '@microblink/blinkcard-in-browser-sdk';
+import React, { SetStateAction } from 'react';
 
 export function getLicenseKeyByEnvironment(env: EMode) {
   switch (env) {
@@ -10,3 +13,125 @@ export function getLicenseKeyByEnvironment(env: EMode) {
       return import.meta.env.VITE_MICROBLINK_LICENSE_KEY_STAGING;
   }
 }
+
+type InitBlinkCardSDK = {
+  setIsShown: React.Dispatch<SetStateAction<boolean>>;
+  setUserMessage: React.Dispatch<SetStateAction<string>>;
+  setFailed: React.Dispatch<SetStateAction<boolean>>;
+  browserErrorString: string;
+  errorString: string;
+};
+
+export const initializeBlinkCardSDK = async ({
+  setIsShown,
+  setFailed,
+  setUserMessage,
+  browserErrorString,
+  errorString,
+}: InitBlinkCardSDK) => {
+  // Check if browser is supported
+  if (BlinkCardSDK.isBrowserSupported()) {
+    const loadSettings = new BlinkCardSDK.WasmSDKLoadSettings(
+      'sRwAAAYZcG12LWludGVncmF0aW9uLmtsaXBzLmRldsP9eD1ByXlqVIXcVwCnhnbT3MP/K8rAxTOoiSgX/uKWOHL26W9Py0HAQ7aT855H8FIOtVAI2Jnh3YuWADLy7n6nWGFvYseyym3KGCxcMki28pk5ND4GGxGk+xgZk1X4pVO35GZaukr1qr/6Fk0QDoN9JIOC6o4VOyNIGwcLrNaf9Cu4bNbB7MpZ'
+    );
+
+    try {
+      const wasmSDK = await BlinkCardSDK.loadWasmModule(loadSettings);
+      setIsShown(true);
+      // The SDK was initialized successfully, you can save the wasmSDK for future use
+      return wasmSDK;
+    } catch (error) {
+      // Error happened during the initialization of the SDK
+      setFailed(true);
+      setUserMessage(errorString);
+      console.error('Error during the initialization of the BlinkCard SDK:', error);
+    }
+  } else {
+    setUserMessage(browserErrorString);
+    console.log('This browser is not supported by the BlinkCard SDK!');
+  }
+};
+
+export enum VERIFICATION_TYPES {
+  PM_VERIFICATION = 'pmVerification',
+}
+
+type PaymentMethodRequestType = {
+  paymentMethodId: string;
+  bin: string;
+  lastDigits: string;
+  expiryYear: number;
+  expiryMonth: number;
+  cardHolder: string;
+  token: string | null;
+};
+
+export type GetActionCodeResponseType = {
+  code: string;
+  expiryTime: number; // in seconds
+};
+
+export type VerifyActionCodeResponseType = {
+  status: boolean;
+};
+
+export const verifyPaymentMethod = async ({
+  paymentMethodId,
+  bin,
+  lastDigits,
+  expiryMonth,
+  expiryYear,
+  cardHolder,
+  token,
+}: PaymentMethodRequestType) =>
+  axios.post(
+    'https://gwcrm-integration.klips.dev/v1/payment/verifyPaymentMethod',
+    {
+      paymentMethodId,
+      bin,
+      lastDigits,
+      expiryYear,
+      expiryMonth,
+      cardHolder,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+export const getActionCode = ({ action, token }: { action: string; token: string }) =>
+  axios.post(
+    'https://gwcrm-integration.klips.dev/v1/user/getActionCode',
+    {
+      action,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
+
+export const validateActionCode = ({
+  action,
+  code,
+  token,
+}: {
+  action: string;
+  code: string;
+  token: string;
+}) =>
+  axios.post(
+    'https://gwcrm-integration.klips.dev/v1/user/validateActionCode',
+    {
+      action,
+      code,
+    },
+    {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    }
+  );
